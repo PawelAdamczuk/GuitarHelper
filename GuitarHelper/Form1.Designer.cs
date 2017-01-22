a7f72752f65cf77254138509f1b2b7a9f8ef9f8c
PawelAdamczuk <p.m.adamczuk@gmail.com>
2017-01-22 19:42:01 +0100

    rozkminione rysowanie, balagan w kodzie, ale przeniose to i tak jak bede mial klasy

--- GuitarHelper/Form1.Designer.cs
+++ GuitarHelper/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tuning";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(937, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chord";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(851, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(498, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "C";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- GuitarHelper/Form1.cs
+++ GuitarHelper/Form1.cs
@@ -16,5 +16,140 @@ namespace GuitarHelper
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            int startingPosX = 70;
+            int startingPosY = 100;
+
+            int fretCount = 12;
+            int stringCount = 6;
+
+            Bitmap background = new Bitmap("..\\..\\res\\background.png");
+            Bitmap strings = new Bitmap("..\\..\\res\\string.png");
+            Bitmap background_top = new Bitmap("..\\..\\res\\background_top.png");
+            Bitmap background_bottom = new Bitmap("..\\..\\res\\background_bottom.png");
+            Bitmap background_left_coverup = new Bitmap("..\\..\\res\\background_left_coverup.png");
+            Bitmap background_right_coverup = new Bitmap("..\\..\\res\\background_right_coverup.png");
+            Bitmap nut = new Bitmap("..\\..\\res\\nut.png");
+
+            for (int i = 0; i < fretCount + 2; i++)
+            {
+                for (int j = 0; j < stringCount; j++)
+                {
+                    e.Graphics.DrawImage(background, startingPosX + i * 60, startingPosY + j * 30, 60, 30);
+                }
+            }
+
+            
+            for (int i = 0; i < fretCount + 2; i++)
+            {
+                for (int j = 0; j < stringCount; j++)
+                {
+                    e.Graphics.DrawImage(strings, startingPosX + i * 60, startingPosY + j * 30, 60, 30);
+                }
+            }
+
+            for (int i = 0; i < stringCount; i++)
+            {
+                e.Graphics.DrawImage(nut, startingPosX, startingPosY + i * 30, 60, 30);
+            }
+
+            
+            for (int i = 0; i < fretCount + 2; i++)
+            {
+                e.Graphics.DrawImage(background_top, startingPosX + i * 60, startingPosY - 16, 60, 30);
+            }
+
+            for (int i = 0; i < fretCount + 2; i++)
+            {
+                e.Graphics.DrawImage(background_bottom, startingPosX + i * 60, startingPosY - 14 + stringCount * 30, 60, 30);
+            }
+
+            for (int i = 0; i < stringCount+2; i++)
+            {
+                e.Graphics.DrawImage(background_left_coverup, startingPosX, startingPosY - 30 + i * 30, 60, 30);
+            }
+
+            for (int i = 0; i < stringCount + 2; i++)
+            {
+                e.Graphics.DrawImage(background_right_coverup, startingPosX + (fretCount + 1) * 60, startingPosY - 30 + i * 30, 60, 30);
+            }
+
+            int startingPosXKeyboard = 70;
+            int startingPosYKeyboard = 400;
+
+            Bitmap whiteKey = new Bitmap("..\\..\\res\\white_key.png");
+            Bitmap blackKey = new Bitmap("..\\..\\res\\black_key.png");
+
+            //6 octaves
+            int keyNumber = 0;
+            int verticalIterator = 0;
+            while (keyNumber < 72)
+            {
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //C
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //D
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //E
+                verticalIterator += 20;
+                keyNumber += 1;
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //F
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //G
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //A
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(whiteKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 20, 100); //B
+                verticalIterator += 20;
+                keyNumber += 1;
+            }
+
+            keyNumber = 1;
+            verticalIterator = 15;
+            while (keyNumber < 72)
+            {
+                e.Graphics.DrawImage(blackKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 10, 60); //C#
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(blackKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 10, 60); //D#
+                verticalIterator += 40;
+                keyNumber += 3;
+                e.Graphics.DrawImage(blackKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 10, 60); //F#
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(blackKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 10, 60); //G#
+                verticalIterator += 20;
+                keyNumber += 2;
+                e.Graphics.DrawImage(blackKey, startingPosXKeyboard + verticalIterator, startingPosYKeyboard, 10, 60); //A#
+                verticalIterator += 40;
+                keyNumber += 3;
+            }
+
+
+
+
+            //e.Graphics.DrawImage(bitmap, 50, 100, 60, 30);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Refresh();
+        }
     }
 }
